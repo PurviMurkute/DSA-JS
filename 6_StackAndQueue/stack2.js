01af1b308270714b6a08ea799e52b3cd622cdea8
PurviMurkute <purvimurkute51@gmail.com>
2026-02-15 22:18:33 +0530

    Implement reverse words in string using stack approach

---
+++ 6_StackAndQueue/stack2.js
@@ -0,0 +1,25 @@
+// Given an input a sentence, reverse the order of the words using a stack. For example, if the input is "Hello World", the output should be "World Hello".
+
+function reverseWords(s) {
+  const arr = s.split(" ");
+  const stack = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    stack.push(arr[i]);
+  }
+
+  let reversed = "";
+
+  while (stack.length) {
+    const current = stack.pop();
+
+    if (current) {
+      reversed += " "+ current;
+    }
+  }
+
+  return reversed.trim();
+}
+
+console.log(reverseWords("     Hello    World    "));      //World Hello
+console.log(reverseWords("the sky is blue"));      //blue is sky the
